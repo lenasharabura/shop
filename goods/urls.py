64a4e5dcd43a24977301899bf0cb1468c234ea1e
Elena <lenasharabura@gmail.com>
2021-11-07 18:53:28 +0200

    edit url

--- goods/urls.py
+++ goods/urls.py
@@ -4,10 +4,14 @@ from rest_framework.routers import DefaultRouter
 from goods.views import *
 
 router = DefaultRouter()
-router.register(r'api/products_list/<slug:slug>', ProductDetailView)
-router.register(r'api/category', CategoryListView)
-router.register(r'api/products_list', ProductsListView)
-router.register(r'api/<slug:slug>', CategoryDetailView)
+router.register(r'api/feedback', FeedbackView)
 
 
-urlpatterns = router.urls
+urlpatterns = [
+
+    path('api/products_list/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('api/category/', CategoryListView.as_view(), name='home_page'),
+    path('api/products_list/', ProductsListView.as_view(), name='products_list'),
+    path('api/<slug:slug>/', CategoryDetailView.as_view(), name='category_product'),
+
+] + router.urls
